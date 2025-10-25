5b278453624bb3a7ef24470d9390dbfb563bb29c
pvrkchan <pvrkchan@naver.com>
2025-10-25 21:02:20 +0900

    test: 기능 구현 목록 테스트에 반영
    기능 구현 목록에 있는 예외사항들과 기능들을 테스트에 반영하였습니다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -13,7 +13,7 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    void 기능_테스트_우승자_하나() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
@@ -24,12 +24,119 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+    void 기능_테스트_우승자_둘() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
         );
     }
+
+    @Test
+    void 기능_테스트_셋_중에_둘_우승() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,java", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "java : -", "최종 우승자 : pobi, java");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 기능_테스트_여러_횟수_시도() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "2");
+                    assertThat(output()).contains("pobi : --", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 예외_테스트_빈_문자열_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차이름_영문자_이외_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java1,자바1", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_콤마가_시작에_위치() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi,java", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_콤마가_끝에_위치() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차이름_한_개_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Test
+    void 예외_테스트_자동차이름_5자_초과() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_콤마_사이_빈_문자열() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,java", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_중복된_이름() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_횟수에_음수_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_횟수에_문자열_포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "5번"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
